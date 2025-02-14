8f555992ec3918da26d56c1cdf1587aaaadef458
Levente Nagy <levente.nagy@clouddirect.net>
2025-02-14 12:30:12 +0100

    updated szolgáltatások

--- app/szolgaltatasok/page.tsx
+++ app/szolgaltatasok/page.tsx
@@ -3,34 +3,39 @@ import Image from "next/image";
 const services = [
   {
     title: "Úszókapuk",
-    description: "Egyedi, modern és tartós úszókapuk gyártása és telepítése.",
+    description:
+      "Úszókapuink modern és praktikus megoldást kínálnak minden olyan ingatlan számára, ahol a helytakarékosság és a könnyű kezelhetőség kiemelt szempont. Az úszókapuk fő előnye, hogy nem igényelnek sínrendszert a talajon, így problémamentesen működnek egyenetlen vagy havas felületeken is. Cégünk kizárólag minőségi alapanyagokat és precíz technológiát alkalmaz a kapuk gyártása és szerelése során, hogy ügyfeleink hosszú távon megbízható, esztétikus és könnyen karbantartható kaput kapjanak. Egyedi igények szerint méretre gyártunk úszókapukat, amelyek modern vagy klasszikus kialakítással is rendelhetők.",
     image: "/1.jpg",
   },
   {
     title: "Kerítések",
-    description: "Esztétikus és masszív kerítések készítése és szerelése.",
+    description:
+      "Egy jól megtervezett és kivitelezett kerítés nemcsak biztonságot nyújt, hanem esztétikai szempontból is meghatározó eleme egy ingatlannak. Különféle típusú kerítések gyártását és telepítését vállaljuk, legyen szó kovácsoltvas, modern fém vagy kombinált fa-fém szerkezetekről. Kiemelten figyelünk arra, hogy az általunk készített kerítések időtállóak, időjárásállóak és minimális karbantartást igénylő megoldások legyenek. Személyre szabott terveinknek köszönhetően minden ügyfelünk olyan kerítést kaphat, amely tökéletesen illeszkedik ingatlanához és egyedi elképzeléseihez.",
     image: "/2.jpg",
   },
   {
-    title: "Lépcsők és Korlátok",
-    description: "Modern és biztonságos acél lépcsők és korlátok gyártása.",
+    title: "Lépcsők",
+    description:
+      "A lépcsők nem csupán funkcionális szerkezetek, hanem esztétikai szempontból is meghatározó elemei egy épületnek. Beltéri és kültéri lépcsők gyártását egyaránt vállaljuk. Az ügyfél igényei alapján modern, letisztult vagy akár díszes kivitelű lépcsőket is készítünk, amelyek strapabíróak, biztonságosak és esztétikusak. Különös figyelmet fordítunk a pontos méretezésre és hogy a lépcső kényelmes és stabil használatot biztosítson hosszú éveken át.",
     image: "/3.jpg",
   },
   {
-    title: "Csarnokok, Garázsok",
-    description: "Nagy teherbírású csarnokok és garázsok kivitelezése.",
+    title: "Korlátok",
+    description:
+      "A korlátok egyik legfontosabb funkciója a biztonság, de emellett meghatározó dizájnelemei is lehetnek egy épületnek. Egyedi fémszerkezetű acél vagy kovácsoltvas korlátok gyártását és telepítését is vállaljuk. Legyen szó beltéri vagy kültéri megoldásokról, minden esetben figyelünk a precíz kivitelezésre, az időjárásállóságra és az esztétikai megjelenésre. Egyedi tervezésünknek köszönhetően modern vagy klasszikus stílusban is elkészítjük a korlátokat, így azok tökéletesen illeszkednek a lépcsőhöz, erkélyhez vagy teraszhoz.",
+    image: "/3.jpg",
+  },
+  {
+    title: "Tárolók és Garázsok",
+    description:
+      "A tárolók, garázsok és kennelek nemcsak praktikus megoldást nyújtanak, hanem esztétikailag is hozzájárulhatnak otthonunk vagy vállalkozásunk rendezett megjelenéséhez. Egyedi igényekre szabott fém és acélszerkezetű tárolók, garázsok és kennelek gyártásával és telepítésével is foglalkozunk. Legyen szó egy kisállat kényelmes és biztonságos elhelyezéséről, járművének védelméről vagy kerti szerszámainak rendszerezett tárolásáról, mi időtálló és esztétikus megoldásokat kínálunk. A precíz kivitelezés és a tartós anyagok garantálják, hogy termékeink hosszú távon megbízhatóan szolgálják Önt. Egyedi tervezésünknek köszönhetően modern és klasszikus stílusban is elkészítjük a szerkezeteket, hogy tökéletesen illeszkedjenek környezetükbe.",
     image: "/4.jpg",
   },
   {
-    title: "Egyedi Acél Szerkezetek",
-    description: "Egyedi acélszerkezetek tervezése és kivitelezése.",
-    image: "/5.jpg",
-  },
-  {
-    title: "Bérhegesztés",
+    title: "Egyéb egyedi acél szerkezetek",
     description:
-      "Professzionális bérhegesztési szolgáltatások egyedi igényekre.",
-    image: "/6.jpg",
+      "Vállalkozásunk egyedi méretre gyártott acélszerkezetek gyártásával és telepítésével is foglalkozik, igazodva az Ön igényeihez és a környezet adottságaihoz. Legyen szó terasz- és előtetőkről, vagy árnyékolókról és pergolákról, valamint eljesen egyéb egyedi szerkezetek kivitelezését is vállaljuk. Ezen kívül egyéb szerkezeteket, állványokat, tartókat is el tudunk készíteni. ",
+    image: "/5.jpg",
   },
 ];
 
