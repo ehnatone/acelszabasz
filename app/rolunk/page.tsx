e61f49553ac8769805dac092e441ce28d70558e7
Levente Nagy <levente.nagy@clouddirect.net>
2025-02-21 19:08:43 +0100

    about text size

--- app/rolunk/page.tsx
+++ app/rolunk/page.tsx
@@ -13,21 +13,23 @@ export default function AboutPage(): JSX.Element {
           {/* Content */}
 
           <div className="relative z-10 container mx-auto p-8 text-white text-center bg-transparent bg-slate-500">
-            <h2 className="text-4xl font-bold py-10">Rólunk</h2>
-            <p className="text-lg my-8">
+            <h2 className="lg:text-4xl md:text-3xl sm:text-2xl font-bold py-10">
+              Rólunk
+            </h2>
+            <p className="lg:text-lg text-md my-8">
               Vállalkozásunk több mint 10 éve foglalkozik úszókapuk, kerítések,
               lépcsők, korlátok és egyéb egyedi lakatos szerkezetek gyártásával.
               Az évek során folyamatosan fejlesztettük technológiánkat és
               bővítettük szolgáltatásainkat, hogy ügyfeleink számára a legjobb
               minőséget biztosítsuk.
             </p>
-            <p className="text-lg my-8">
+            <p className="lg:text-lg text-md  my-8">
               Székesfehérvár mellett, Zámolyon működünk, és körülbelül 100 km-es
               körzetben vállalunk megbízásokat. Ügyfeleink könnyen elérhetnek
               minket telefonon és e-mailben, és kiemelten figyelünk a tiszta,
               egyértelmű kommunikációra.
             </p>
-            <p className="text-lg my-8">
+            <p className="lg:text-lg text-md  my-8">
               Több száz sikeresen elkészült projekt és számtalan elégedett
               ügyfél igazolja szakértelmünket. Referenciamunkáinkról a
               galériában számos kép található. Ha tartós és igényes megoldást
